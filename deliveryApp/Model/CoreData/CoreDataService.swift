206a6ae5c677a0a2e1f7ad5ba81bf8cbd6e6e692
Vladimir Ratskevich <76956168+vovaratskevich@users.noreply.github.com>
2021-11-29 18:59:45 +0300

    Add methods to CoreDataService

--- deliveryApp/Model/CoreData/CoreDataService.swift
+++ deliveryApp/Model/CoreData/CoreDataService.swift
@@ -10,12 +10,12 @@ import CoreData
 
 class CoreDataService {
     
-    var context: NSManagedObjectContext{
+    var context: NSManagedObjectContext    {
         return persistentContainer.viewContext
     }
     
-    private lazy var persistentContainer: NSPersistentContainer = {
-            let container = NSPersistentContainer(name: "nordgold_hr_services")
+    lazy var persistentContainer: NSPersistentContainer = {
+            let container = NSPersistentContainer(name: "deliveryApp")
             container.loadPersistentStores { description, error in
                 if let error = error {
                     fatalError("Unable to load persistent stores: \(error)")
@@ -24,12 +24,28 @@ class CoreDataService {
             return container
     }()
     
-    private func saveIfContextHasChanges() {
+    func saveIfContextHasChanges() {
         if context.hasChanges {
             do {
                 try context.save()
             } catch {
                 debugPrint(error.localizedDescription)
+            }
+        }
+    }
+    
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
         }
     }
